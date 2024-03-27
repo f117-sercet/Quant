0f503ab769d473565ab5a7ad76a3c9f6b0920580
sowrd <15159885771@163.com>
2024-03-27 09:51:08 +0800

    连接mongo

--- base/base03.py
+++ base/base03.py
@@ -1,6 +1,12 @@
-import akshare as ak
+
+import pymongo
 
 if __name__ == '__main__':
     # 获取可转债代码列表
-    conbon_list = ak.bond_zh_cov()['债券代码'].tolist()
-    #print(conbon_list)
+    # conbon_list = ak.bond_zh_cov()['转债代码'].tolist()
+    # print(conbon_list)
+
+    # 连接mongo
+    client = pymongo.MongoClient(host='localhost', port=27017)
+    dbs=client.list_database_names()
+    print(dbs)
